16472f16f6aaf4a9a1cf4dc17860868d8abb41b1
illusion01 <131750826+pratahmesh@users.noreply.github.com>
2023-12-06 20:01:17 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,8 +3,7 @@ import os
 import math
 import sys
 
-
-def create_combined_image(directory_path, base_name, produce_2x):
+def create_combined_image(directory_path, base_name, produce_1x):
     # Get a list of all PNG files in the directory
     png_files = [f for f in os.listdir(directory_path) if f.endswith(".png")]
     if not png_files:
@@ -12,172 +11,141 @@ def create_combined_image(directory_path, base_name, produce_2x):
         return
 
     # Create a list to store Image objects
-    images_1x = []
-    images_2x = []
-
-    # Open each PNG file, downscale a little for 1x, and append it to the list
+    images = []
+    
+    # Open each PNG file and append it to the list
     for png_file in png_files:
         file_path = os.path.join(directory_path, png_file)
         image = Image.open(file_path)
+        images.append(image)
 
-        # Downscale each image a little for 1x
-        downscale_factor_1x = 0.6  # Adjust as needed
-        target_size_1x = (
-            int(image.width * downscale_factor_1x),
-            int(image.height * downscale_factor_1x),
-        )
-        image_1x = image.resize(target_size_1x, Image.ANTIALIAS)
-        images_1x.append(image_1x)
-
-        if produce_2x:
-            # Downscale even more for 2x
-            downscale_factor_2x = 0.3  # Adjust as needed
-            target_size_2x = (
-                max(1, int(image.width * downscale_factor_2x)),
-                max(1, int(image.height * downscale_factor_2x)),
-            )
-            image_2x = image.resize(target_size_2x, Image.ANTIALIAS)
-            images_2x.append(image_2x)
-
-    # Resize all 2x images to have the same dimensions
-    if produce_2x:
-        max_width_2x = max(image.width for image in images_2x)
-        max_height_2x = max(image.height for image in images_2x)
-
-        images_2x_resized = [
-            image.resize((max_width_2x, max_height_2x), Image.ANTIALIAS)
-            for image in images_2x
-        ]
-
-    total_images = len(images_1x)
+    total_images = len(images)
     rows = int(math.sqrt(total_images))
     columns = math.ceil(total_images / rows)
 
-    # Calculate the dimensions of each cell in the grid for 1x
-    cell_width_1x = max(image.width for image in images_1x)
-    cell_height_1x = max(image.height for image in images_1x)
+    # Calculate the dimensions of each cell in the grid
+    cell_width = max(image.width for image in images)
+    cell_height = max(image.height for image in images)
 
-    # Create a new blank image with the calculated dimensions for the 1x grid
-    grid_width_1x = columns * cell_width_1x
-    grid_height_1x = rows * cell_height_1x
-    combined_image_1x = Image.new("RGB", (grid_width_1x, grid_height_1x), (255, 255, 255))
+    # Create a new blank image with the calculated dimensions for the grid
+    grid_width = columns * cell_width
+    grid_height = rows * cell_height
+    combined_image = Image.new("RGB", (grid_width, grid_height), (255, 255, 255))
 
-    # Paste each downscale 1x image onto the combined 1x image in a grid pattern
+    # Paste each original image onto the combined image in a grid pattern
     current_x = 0
     current_y = 0
-    for image_1x in images_1x:
-        combined_image_1x.paste(image_1x, (current_x, current_y))
-        current_x += cell_width_1x
-        if current_x >= grid_width_1x:
+    for image in images:
+        combined_image.paste(image, (current_x, current_y))
+        current_x += cell_width
+        if current_x >= grid_width:
             current_x = 0
-            current_y += cell_height_1x
+            current_y += cell_height
 
-    # Save the combined 1x image
-    combined_image_1x_path = os.path.join(directory_path, f"{base_name}.png")
-    combined_image_1x.save(combined_image_1x_path)
-    print(f"Combined grid image saved successfully at {combined_image_1x_path}.")
+    # Save the 2x combined image
+    combined_image_path = os.path.join(directory_path, f"{base_name}.png")
+    combined_image.save(combined_image_path)
+    print(f"2x Combined grid image saved successfully at {combined_image_path}.")
 
-    # Save the 2x image if requested
-    if produce_2x:
-        # Calculate the dimensions of each cell in the grid for 2x
-        cell_width_2x = max_width_2x
-        cell_height_2x = max_height_2x
+    # Save the 1x image if requested
+    if produce_1x:
+        downscaled_images = [image.resize((image.width // 2, image.height // 2), Image.ANTIALIAS) for image in images]
+        grid_width_1x = columns * (cell_width // 2)
+        grid_height_1x = rows * (cell_height // 2)
+        combined_image_1x = Image.new("RGB", (grid_width_1x, grid_height_1x), (255, 255, 255))
 
-        # Create a new blank image with the calculated dimensions for the 2x grid
-        grid_width_2x = columns * cell_width_2x
-        grid_height_2x = rows * cell_height_2x
-        combined_image_2x = Image.new("RGB", (grid_width_2x, grid_height_2x), (255, 255, 255))
-
-        # Paste each resized 2x image onto the combined 2x image in a grid pattern
+        # Paste each downscaled image onto the combined image in a grid pattern
         current_x = 0
         current_y = 0
-        for image_2x in images_2x_resized:
-            combined_image_2x.paste(image_2x, (current_x, current_y))
-            current_x += cell_width_2x
-            if current_x >= grid_width_2x:
+        for downscaled_image in downscaled_images:
+            combined_image_1x.paste(downscaled_image, (current_x, current_y))
+            current_x += cell_width // 2
+            if current_x >= grid_width_1x:
                 current_x = 0
-                current_y += cell_height_2x
+                current_y += cell_height // 2
 
-        # Save the 2x combined image
-        combined_image_2x_path = os.path.join(directory_path, f"{base_name}@2x.png")
-        combined_image_2x.save(combined_image_2x_path)
-        print(f"2x Combined grid image saved successfully at {combined_image_2x_path}.")
+        # Save the 1x combined image
+        combined_image_1x_path = os.path.join(directory_path, f"{base_name}@1x.png")
+        combined_image_1x.save(combined_image_1x_path)
+        print(f"1x Combined grid image saved successfully at {combined_image_1x_path}.")
 
-    # CSS sprite file
+
+
     css_sprite_path = os.path.join(directory_path, f"{base_name}.css")
     with open(css_sprite_path, "w") as css_file:
-        # Write common styles for all platforms
-        css_file.write(f".sprite-game.{base_name} {{\n")
-        css_file.write("    display: inline-block;\n")
-        css_file.write(f'    background-image: url("{base_name}.png");\n')
-        css_file.write(f'    background-size: {grid_width_1x}px {grid_height_1x}px;\n')
-        css_file.write("}\n\n")
+      # Write common styles for all platforms
+      css_file.write(f".sprite-game.{base_name} {{\n")
+      css_file.write("  display: inline-block;\n")
+      css_file.write(f'  background-image: url("{base_name}.png");\n')
+      css_file.write(f'  background-size: {grid_width}px {grid_height}px;\n')
+      css_file.write("}\n\n")
 
-        # Create CSS sprite file with media queries and individual class names
-        css_file.write(f'@media screen and (-webkit-min-device-pixel-ratio: 1), '
-                      f'screen and (-o-min-device-pixel-ratio: 100/100), '
-                      f'screen and (min-device-pixel-ratio: 1), '
-                      f'screen and (-o-min-device-pixel-ratio: 1/1), '
-                      f'screen and (min-resolution: 1dppx) {{\n')
+      # Create CSS sprite file with media queries and individual class names
+      css_file.write(f'@media screen and (-webkit-min-device-pixel-ratio: 1), '
+                    f'screen and (-o-min-device-pixel-ratio: 100/100), '
+                    f'screen and (min-device-pixel-ratio: 1), '
+                    f'screen and (-o-min-device-pixel-ratio: 1/1), '
+                    f'screen and (min-resolution: 1dppx) {{\n')
+      css_file.write(f'  .sprite-games.{base_name} {{\n')
+      css_file.write(f'    background-image: url("{base_name}.png");\n')
+      css_file.write(f'    background-size: {grid_width}px {grid_height}px;\n')
+      css_file.write("  }\n}\n\n")
+
+      # Write platform-specific styles for 1x resolution
+      if produce_1x:
+        css_file.write(
+            "@media screen and (-webkit-min-device-pixel-ratio: 2),\n")
+        css_file.write("  screen and (-o-min-device-pixel-ratio: 200/100),\n")
+        css_file.write("  screen and (min-device-pixel-ratio: 2),\n")
+        css_file.write("  screen and (-o-min-device-pixel-ratio: 2/1),\n")
+        css_file.write("  screen and (min-resolution: 2dppx) {\n")
         css_file.write(f'  .sprite-games.{base_name} {{\n')
-        css_file.write(f'    background-image: url("{base_name}.png");\n')
-        css_file.write(f'    background-size: {grid_width_1x}px {grid_height_1x}px;\n')
-        css_file.write("  }\n}\n\n")
-
-        # Write platform-specific styles for 2x resolution
-        if produce_2x:
-            css_file.write(
-                "@media screen and (-webkit-min-device-pixel-ratio: 2),\n")
-            css_file.write("  screen and (-o-min-device-pixel-ratio: 200/100),\n")
-            css_file.write("  screen and (min-device-pixel-ratio: 2),\n")
-            css_file.write("  screen and (-o-min-device-pixel-ratio: 2/1),\n")
-            css_file.write("  screen and (min-resolution: 2dppx) {\n")
-            css_file.write(f'  .sprite-games.{base_name} {{\n')
-            css_file.write(f'    background-image: url("{base_name}@2x.png");\n')
-            css_file.write(
-                f'    background-size: {2 * grid_width_1x}px {2 * grid_height_1x}px;\n')
-            css_file.write("  }\n")
-            css_file.write("}\n\n")
-
-        # Write platform-specific styles for other platforms
+        css_file.write(f'    background-image: url("{base_name}@1x.png");\n')
         css_file.write(
-            "@media screen and (-webkit-min-device-pixel-ratio: /* platform ratio */),\n"
-        )
-        css_file.write(
-            "  screen and (-o-min-device-pixel-ratio: /* platform ratio/100 */),\n"
-        )
-        css_file.write(
-            "  screen and (min-device-pixel-ratio: /* platform ratio */),\n")
-        css_file.write(
-            "  screen and (-o-min-device-pixel-ratio: /* platform ratio/1 */),\n")
-        css_file.write(
-            "  screen and (min-resolution: /* platform resolution */dppx) {\n")
-        css_file.write("  .sprite-game {\n")
-        css_file.write(
-            f"    background-image: url('../..//images/other-platform.png');\n")
-        css_file.write("    /* Additional styles for other platforms */\n")
+            f'    background-size: {2 * grid_width}px {2 * grid_height}px;\n')
         css_file.write("  }\n")
         css_file.write("}\n\n")
 
-        for image_1x in images_1x:
-            file_name = os.path.splitext(os.path.basename(png_files[images_1x.index(image_1x)]))[0]
-            css_class = f".sprite-games.{base_name}.{file_name}{{\n"
-            css_class += f"  width: {image_1x.width}px;\n"
-            css_class += f"  height: {image_1x.height}px;\n"
-            css_class += f"  background-position: {-current_x}px {-current_y}px;\n"
-            css_class += "}\n\n"
-            css_file.write(css_class)
+      # Write platform-specific styles for other platforms
+      css_file.write(
+          "@media screen and (-webkit-min-device-pixel-ratio: /* platform ratio */),\n"
+      )
+      css_file.write(
+          "  screen and (-o-min-device-pixel-ratio: /* platform ratio/100 */),\n"
+      )
+      css_file.write(
+          "  screen and (min-device-pixel-ratio: /* platform ratio */),\n")
+      css_file.write(
+          "  screen and (-o-min-device-pixel-ratio: /* platform ratio/1 */),\n")
+      css_file.write(
+          "  screen and (min-resolution: /* platform resolution */dppx) {\n")
+      css_file.write("  .sprite-game {\n")
+      css_file.write(
+          f"    background-image: url('../..//images/other-platform.png');\n")
+      css_file.write("    /* Additional styles for other platforms */\n")
+      css_file.write("  }\n")
+      css_file.write("}\n\n")
+
+      for image in images:
+        file_name = os.path.splitext(os.path.basename(image.filename))[0]
+        css_class = f".sprite-games.{base_name}.{file_name}{{\n"
+        css_class += f"  width: {image.width}px;\n"
+        css_class += f"  height: {image.height}px;\n"
+        css_class += f"  background-position: {-current_x}px {-current_y}px;\n"
+        css_class += "}\n\n"
+        css_file.write(css_class)
 
     print(f"CSS sprite file saved successfully at {css_sprite_path}.")
 
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python script.py <directory_path> <base_name> <produce_2x>")
+        print("Usage: python script.py <directory_path> <base_name> <produce_1x>")
         sys.exit(1)
 
     directory_path = sys.argv[1]
     base_name = sys.argv[2]
-    produce_2x = bool(int(sys.argv[3]))
+    produce_1x = bool(int(sys.argv[3]))
 
-    create_combined_image(directory_path, base_name, produce_2x)
+    create_combined_image(directory_path, base_name, produce_1x)
